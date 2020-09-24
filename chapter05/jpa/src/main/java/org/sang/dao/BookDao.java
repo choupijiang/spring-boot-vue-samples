bd0d535e49d6490b8f8382913717806e3defc14f
zhangshenghu <zhangshenghu@sensetime.com>
2020-09-24 22:31:09 +0800

    t_book -> Book

--- chapter05/jpa/src/main/java/org/sang/dao/BookDao.java
+++ chapter05/jpa/src/main/java/org/sang/dao/BookDao.java
@@ -15,8 +15,8 @@ public interface BookDao extends JpaRepository<Book,Integer>{
     List<Book> getBooksByPriceGreaterThan(Float price);
     @Query(value = "select * from t_book where id=(select max(id) from t_book)",nativeQuery = true)
     Book getMaxIdBook();
-    @Query("select b from t_book b where b.id>:id and b.author=:author")
+    @Query("select b from Book b where b.id>:id and b.author=:author")
     List<Book> getBookByIdAndAuthor(@Param("author") String author, @Param("id") Integer id);
-    @Query("select b from t_book b where b.id<?2 and b.name like %?1%")
+    @Query("select b from Book b where b.id<?2 and b.name like %?1%")
     List<Book> getBooksByIdAndName(String name, Integer id);
 }
